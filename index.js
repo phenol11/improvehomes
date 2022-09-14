3f30849fedb4d26ca3a915dc70ea42800d0678da
phenol11 <101388672+phenol11@users.noreply.github.com>
2022-09-14 21:10:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+$('.row').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa-solid fa-arrow-left left-arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-arrow-right right-arrow"></i>'
+});
+
+$('.row2').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa-solid fa-arrow-left left-arrow2"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-right right-arrow2"></i>'
+  });
+//   $('.row-b').slick({
+//     lazyLoad: 'ondemand',
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     prevArrow: '<i class="fa-solid fa-arrow-left left-arrow"></i>',
+//     nextArrow: '<i class="fa-solid fa-arrow-right right-arrow"></i>'
+//   });
+  
